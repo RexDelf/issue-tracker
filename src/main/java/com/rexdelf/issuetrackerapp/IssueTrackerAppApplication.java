9afb9bffd60836c72195cbbfa05ca4522845001a
Dominic <jadoro03@gmail.com>
2023-07-05 14:15:46 +0300

    fix boot application formatting

--- src/main/java/com/rexdelf/issuetrackerapp/IssueTrackerAppApplication.java
+++ src/main/java/com/rexdelf/issuetrackerapp/IssueTrackerAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IssueTrackerAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IssueTrackerAppApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(IssueTrackerAppApplication.class, args);
+  }
 
 }
