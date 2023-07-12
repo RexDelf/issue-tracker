0f37bd18403f55b3d8aec2e16207f6060e831ec3
Dominic <jadoro03@gmail.com>
2023-07-12 12:35:09 +0300

    add nested entities to TicketDto

--- src/main/java/com/rexdelf/issuetrackerapp/mapper/TicketMapper.java
+++ src/main/java/com/rexdelf/issuetrackerapp/mapper/TicketMapper.java
@@ -8,16 +8,10 @@ import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface TicketMapper {
-  @Mapping(target = "priority", source = "priority.name")
-  @Mapping(target = "status", source = "status.name")
-  @Mapping(target = "assigneeName", source = "assignee.name")
-  @Mapping(target = "assigneeSurname", source = "assignee.surname")
-  @Mapping(target = "reporterName", source = "reporter.name")
-  @Mapping(target = "reporterSurname", source = "reporter.surname")
+
   TicketDto ticketToTicketDto(Ticket ticket);
 
   Ticket ticketPostDtoToTicket(TicketPostDto ticketPostDto);
