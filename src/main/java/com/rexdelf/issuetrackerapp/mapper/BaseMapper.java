648f15ee2b846d4630b21fa3f19a145a278e1685
Dominic <jadoro03@gmail.com>
2023-07-19 13:11:49 +0300

    make date mapping null-safe

--- src/main/java/com/rexdelf/issuetrackerapp/mapper/BaseMapper.java
+++ src/main/java/com/rexdelf/issuetrackerapp/mapper/BaseMapper.java
@@ -3,11 +3,14 @@ package com.rexdelf.issuetrackerapp.mapper;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
+import java.util.Optional;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface BaseMapper {
   default OffsetDateTime mapToOffsetDateTime(LocalDateTime dateTime){
-    return OffsetDateTime.of(dateTime, ZoneOffset.UTC);
+    return Optional.ofNullable(dateTime)
+        .map(dt -> OffsetDateTime.of(dt, ZoneOffset.UTC))
+        .orElse(null);
   }
 }
